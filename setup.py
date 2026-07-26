6549d9f18a6f93fc2e2f9e012cfe320e3e747eca
Mehrdad Ansarifar <mehrdad.devv@gmail.com>
2026-07-26 16:31:08 +0330

    Fix formatting in setup.py for consistency

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="aizen-todo",
     version="2.0.0",
-    packages=find_packages(include=["aizen_todo", "aizen_todo.*"]),
-    install_requires=["rich>=13.0", "click>=8.0"],
+    packages=find_packages(include=["aizen_todo","aizen_todo.*"]),
+    install_requires=["rich>=13.0","click>=8.0"],
     entry_points={
         "console_scripts": [
             "aizen-todo = aizen_todo.__main__:main",
